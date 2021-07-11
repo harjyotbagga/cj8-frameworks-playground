ec19239895a513fbc05df197d2d7e48ac7823200
harjyotbagga <imharjyotbagga@gmail.com>
2021-07-11 16:53:07 +0530

    asciimatics framework

---
+++ asciimatics/demo1.py
@@ -0,0 +1,20 @@
+from asciimatics.screen import Screen
+from asciimatics.scene import Scene
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
+
+def demo(screen):
+    effects = [
+        Cycle(
+            screen,
+            FigletText("ASCIIMATICS", font='big'),
+            screen.height // 2 - 8),
+        Cycle(
+            screen,
+            FigletText("ROCKS!", font='big'),
+            screen.height // 2 + 3),
+        Stars(screen, (screen.width + screen.height) // 2)
+    ]
+    screen.play([Scene(effects, 500)])
+
+Screen.wrapper(demo)
